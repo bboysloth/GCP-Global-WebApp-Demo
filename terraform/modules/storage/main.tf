d40a112ad7c709af4ee892d36e0a6bf1df450c7f
bboysloth <jeremyhannan@gmail.com>
2024-12-08 16:33:53 -0700

    Update main.tf
    updated bucket creation syntax

--- terraform/modules/storage/main.tf
+++ terraform/modules/storage/main.tf
@@ -1,1 +1,39 @@
+# Create Regional Bucket for Static Content in the US
 
+resource "google_storage_bucket" "bucket_us" {
+  name     = "gcp-simple-global-web-app-demo-us-${var.project_id}" # Using project ID for unique naming
+  project  = var.project_id
+  location = "US-CENTRAL1"
+  uniform_bucket_level_access = true
+
+  website {
+    main_page_suffix = "index.html"
+    not_found_page   = "404.html"
+  }
+}
+
+# Create Regional bucket for static content in Europe
+
+resource "google_storage_bucket" "bucket_europe" {
+  name     = "gcp-simple-global-web-app-demo-europe-${var.project_id}" # Using project ID for unique naming
+  project  = var.project_id
+  location = "EUROPE-WEST1"
+  uniform_bucket_level_access = true
+
+  website {
+    main_page_suffix = "index.html"
+    not_found_page   = "404.html"
+  }
+}
+
+resource "google_storage_bucket_object" "object_us" {
+    name = "image.png"
+    bucket = google_storage_bucket.bucket_us.name
+    source = "image.png"
+}
+
+resource "google_storage_bucket_object" "object_europe" {
+    name = "image.png"
+    bucket = google_storage_bucket.bucket_europe.name
+    source = "image.png"
+}
