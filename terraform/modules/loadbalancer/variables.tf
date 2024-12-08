5dda9611e7abf485de78fae145e693f3a5f5659f
bboysloth <jeremyhannan@gmail.com>
2024-12-08 16:55:56 -0700

    Update variables.tf
    updated variables for Loadbalancers

--- terraform/modules/loadbalancer/variables.tf
+++ terraform/modules/loadbalancer/variables.tf
@@ -1,1 +1,29 @@
+variable "project_id" {
+    description = "The project ID for the load balancer"
+    type = string
+}
 
+variable "region" {
+    description = "The default region for the load balancer"
+    type = string
+}
+
+variable "mig_us_self_link" {
+    description = "The self link of the MIG in the US"
+    type = string
+}
+
+variable "mig_europe_self_link" {
+    description = "The self link of the MIG in Europe"
+    type = string
+}
+
+variable "subnet_us_id" {
+    description = "The ID of the subnet in the us region"
+    type = string
+}
+
+variable "subnet_europe_id" {
+    description = "The ID of the subnet in the europe region"
+    type = string
+}
