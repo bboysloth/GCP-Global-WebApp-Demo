383715d8b4fd6e27ca6712e6e50098ea8ea2f4a9
bboysloth <jeremyhannan@gmail.com>
2024-12-08 16:27:30 -0700

    Update variables.tf

--- terraform/modules/compute/variables.tf
+++ terraform/modules/compute/variables.tf
@@ -1,3 +1,13 @@
+variable "region" {
+  description = "The region where instances will be created"
+  type        = string
+}
+
+variable "project_id" {
+    description = "The project where instances will be created"
+    type = string
+}
+
 variable "subnet_id" {
   description = "The ID of the subnet where instances will be created"
   type = string
