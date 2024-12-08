e38c181cdd95e7fa5259d185fea42544e3350577
bboysloth <jeremyhannan@gmail.com>
2024-12-08 16:28:05 -0700

    Update outputs.tf
    added outputs to code for later usage

--- terraform/modules/compute/outputs.tf
+++ terraform/modules/compute/outputs.tf
@@ -1,1 +1,11 @@
+output "instance_template_id" {
+  value = google_compute_instance_template.default.id
+}
 
+output "mig_us_self_link" {
+  value = google_compute_region_instance_group_manager.us_mig.self_link
+}
+
+output "mig_europe_self_link" {
+  value = google_compute_region_instance_group_manager.europe_mig.self_link
+}
