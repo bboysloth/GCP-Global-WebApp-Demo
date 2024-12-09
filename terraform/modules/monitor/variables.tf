29ed42f360d67dc75fc863dcbe3793a5179a09b3
bboysloth <jeremyhannan@gmail.com>
2024-12-08 17:18:35 -0700

    Update variables.tf

--- terraform/modules/monitor/variables.tf
+++ terraform/modules/monitor/variables.tf
@@ -1,1 +1,9 @@
+variable "project_id" {
+  description = "The ID of the project where the monitoring instance will be created"
+  type        = string
+}
 
+variable "subnet_id" {
+  description = "The ID of the subnet where the monitoring instance will be created"
+  type        = string
+}
