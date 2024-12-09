b77e6a23bf740e87bb4dfdf8fbfb0742707477ed
bboysloth <jeremyhannan@gmail.com>
2024-12-09 08:34:33 -0700

    Update main.tf

--- terraform/modules/network/main.tf
+++ terraform/modules/network/main.tf
@@ -1,30 +1,30 @@
 resource "google_compute_network" "vpc_network" {
-  project = var.project_id
-  name = "terraform-demo-vpc"
-  auto_create_subnetworks = false # We'll create custom subnetworks
+  project                 = var.project_id
+  name                    = "terraform-demo-vpc"
+  auto_create_subnetworks = false
 }
 
 resource "google_compute_subnetwork" "subnet_us" {
-  project = var.project_id
-  name          = "terraform-demo-subnet-us"
-  ip_cidr_range = "10.0.1.0/24"
-  region        = "us-central1"
+  project                    = var.project_id
+  name                        = "terraform-demo-subnet-us"
+  ip_cidr_range              = "10.0.1.0/24"
+  region = "us-central1"
   network       = google_compute_network.vpc_network.id
-
 }
 
 resource "google_compute_subnetwork" "subnet_europe" {
-  project = var.project_id
-  name          = "terraform-demo-subnet-europe"
-  ip_cidr_range = "10.0.2.0/24"
-  region        = "europe-west1"
+  project                    = var.project_id
+  name = "terraform-demo-subnet-europe"
+  ip_cidr_range              = "10.0.2.0/24"
+ region = "europe-west1"
   network       = google_compute_network.vpc_network.id
+
 }
 
 resource "google_compute_subnetwork" "subnet_asia" {
-  project = var.project_id
+ project                    = var.project_id
   name          = "terraform-demo-subnet-asia"
-  ip_cidr_range = "10.0.3.0/24"
-  region        = "asia-southeast1"
+ ip_cidr_range              = "10.0.3.0/24"
+ region = "asia-southeast1"
   network       = google_compute_network.vpc_network.id
 }
