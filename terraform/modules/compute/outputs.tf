7ccb8c911adba920ea1cbdb0824c9ff7ff579c68
bboysloth <jeremyhannan@gmail.com>
2024-12-09 08:29:50 -0700

    Update outputs.tf

--- terraform/modules/compute/outputs.tf
+++ terraform/modules/compute/outputs.tf
@@ -1,7 +1,3 @@
-output "instance_template_id" {
-    value = google_compute_instance_template.default.self_link
-}
-
 output "instance_group" {
     value = google_compute_region_instance_group_manager.mig.instance_group
 }
