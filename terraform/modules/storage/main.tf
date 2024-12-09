86da25371698131a8be776e50200f0d63a369158
bboysloth <jeremyhannan@gmail.com>
2024-12-09 08:30:25 -0700

    Update main.tf

--- terraform/modules/storage/main.tf
+++ terraform/modules/storage/main.tf
@@ -1,35 +1,29 @@
 resource "google_storage_bucket" "default" {
-  project  = var.project_id
-  name                        = "gcp-simple-global-web-app-demo-${var.project_id}"
-  location = "US" # Set a consistent location for simplicity
-  uniform_bucket_level_access = true
+ project  = var.project_id
+ name                        = "gcp-simple-global-web-app-demo-${var.project_id}" # Simplified bucket name
+ location = "US"  # Fixed location for simplicity
+ uniform_bucket_level_access = true
 
-  website {
-    main_page_suffix = "index.html"
-    not_found_page   = "404.html"
-  }
+ website {
+   main_page_suffix = "index.html"
+   not_found_page   = "404.html"
+ }
 }
 
 resource "google_storage_bucket_object" "object_us" {
-    name = "image_bullet.jpg"
-    bucket = google_storage_bucket.default.name
-
-    source = "${path.module}/images/image_bullet.jpg"
-
+ name   = "image_bullet.jpg"
+ bucket = google_storage_bucket.default.name #Reference single bucket
+ source = "${path.module}/images/image_bullet.jpg"
 }
 
 resource "google_storage_bucket_object" "object_europe" {
-    name = "image_dome.jpg"
-
- bucket = google_storage_bucket.default.name
-    source = "${path.module}/images/image_dome.jpg"
-
+ name   = "image_dome.jpg"
+ bucket = google_storage_bucket.default.name #Reference single bucket
+ source = "${path.module}/images/image_dome.jpg"
 }
 
 resource "google_storage_bucket_object" "object_asia" {
-
-    name = "image_asia.jpg"
- bucket = google_storage_bucket.default.name
-    source = "${path.module}/images/image_asia.jpg"
-
+ name   = "image_asia.jpg"
+ bucket = google_storage_bucket.default.name #Reference single bucket
+ source = "${path.module}/images/image_asia.jpg"
 }
