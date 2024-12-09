9bf7624ffb686026c7dfe43ed117ab1439767302
bboysloth <jeremyhannan@gmail.com>
2024-12-08 20:41:03 -0700

    Update main.tf
    overhaul update

--- terraform/modules/compute/main.tf
+++ terraform/modules/compute/main.tf
@@ -1,52 +1,33 @@
 resource "google_compute_instance_template" "default" {
-  name           = "web-server-template"
-  machine_type   = "e2-micro"
+  name_prefix  = "web-server-template-${var.region}-" # Make names unique per region
+  machine_type = "e2-micro"
   disk {
     source_image = "debian-cloud/debian-11"
     auto_delete  = true
     boot         = true
   }
-
-  network_interface { # Network interface should be defined here in the instance template
+  network_interface {
     subnetwork = var.subnet_id
   }
 
   metadata = {
     startup-script = templatefile("${path.module}/../../../scripts/startup_script.sh", {
-      project_id = var.project_id
+      project_id = var.project_id,
+      region     = var.region # Pass the region to the startup script
     })
   }
 }
 
-resource "google_compute_region_instance_group_manager" "us_mig" {
-  provider = google-beta
-  name     = "web-server-mig-us"
-  region   = "us-central1"
-  project = var.project_id
+resource "google_compute_region_instance_group_manager" "mig" { # Removed us and europe from name
+  provider              = google-beta
+  name                  = "web-server-mig-${var.region}"  # Make names unique per region
+  region                = var.region
+  project               = var.project_id
+  wait_for_instances    = false
+  base_instance_name    = "web-server-${var.region}"
 
   version {
-    instance_template = google_compute_instance_template.default.id
     name              = "primary"
-  }
-
-  base_instance_name = "web-server-us"
-
-  wait_for_instances = false # Added this line to prevent errors
-
-}
-
-resource "google_compute_region_instance_group_manager" "europe_mig" {
-  provider = google-beta
-  name     = "web-server-mig-europe"
-  region   = "europe-west1"
-  project = var.project_id
-
-  version {
     instance_template = google_compute_instance_template.default.id
-    name              = "primary"
   }
-
-  base_instance_name = "web-server-europe"
-
-  wait_for_instances = false
 }
