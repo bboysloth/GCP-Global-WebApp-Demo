1172734b47a83d30b6c4806109eb421a4ea3b1e6
bboysloth <jeremyhannan@gmail.com>
2024-12-08 17:09:21 -0700

    Update main.tf
    updated script to be outside of this file to avoid TF confusion when interpreting variables.

--- terraform/modules/compute/main.tf
+++ terraform/modules/compute/main.tf
@@ -10,30 +10,11 @@ resource "google_compute_instance_template" "default" {
     subnetwork = var.subnet_id
   }
 
-# startup script can be moved to a seperate file later
-    metadata_startup_script = <<-EOF
-        #!/bin/bash
-        sudo apt-get update
-        sudo apt-get install apache2 -y
-        sudo a2enmod rewrite
-        cat <<EOT > /etc/apache2/sites-available/000-default.conf
-        <VirtualHost *:80>
-            ServerAdmin webmaster@localhost
-            DocumentRoot /var/www/html
-
-            <Directory /var/www/html/>
-                RewriteEngine On
-                RewriteCond %{REQUEST_URI} !^/index.html$
-                RewriteRule ^(.*)$ /index.html [L]
-            </Directory>
-
-            ErrorLog ${APACHE_LOG_DIR}/error.log
-            CustomLog ${APACHE_LOG_DIR}/access.log combined
-        </VirtualHost>
-        EOT
-        sudo systemctl restart apache2
-        echo "<h1>Hello from $(hostname) in $(curl -s http://metadata.google.internal/computeMetadata/v1/instance/zone -H 'Metadata-Flavor: Google')</h1><img src='https://storage.googleapis.com/gcp-simple-global-web-app-demo-us-${var.project_id}/image_bullet.jpg'>" > /var/www/html/index.html
-        EOF
+# startup script moved to main/scripts/startup_script.sh
+      metadata_from_file = {
+    startup-script = "../../../scripts/startup_script.sh"
+  }
+}
 }
 
 resource "google_compute_region_instance_group_manager" "us_mig" {
