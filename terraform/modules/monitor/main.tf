44d2f92f34ff8692815c3fb7278dc38f08ca8097
bboysloth <jeremyhannan@gmail.com>
2024-12-08 17:18:21 -0700

    Update main.tf

--- terraform/modules/monitor/main.tf
+++ terraform/modules/monitor/main.tf
@@ -1,1 +1,16 @@
+resource "google_compute_instance" "monitoring_instance" {
+  project      = var.project_id
+  name         = "monitoring-instance"
+  machine_type = "e2-micro"
+  zone         = "asia-southeast1-a"
 
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    subnetwork = var.subnet_id
+  }
+}
