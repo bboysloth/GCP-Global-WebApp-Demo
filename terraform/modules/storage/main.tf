7d6b29b80859104d40b96d2c0756ef1056fe82c1
bboysloth <jeremyhannan@gmail.com>
2024-12-08 21:24:52 -0700

    Update main.tf

--- terraform/modules/storage/main.tf
+++ terraform/modules/storage/main.tf
@@ -1,22 +1,18 @@
-resource "google_storage_bucket" "bucket" {  # Removed "_us" suffix, will append region later
+resource "google_storage_bucket" "default" {
   project  = var.project_id
-
-  # Dynamically create bucket name per region using variable
-  name                        = "gcp-simple-global-web-app-demo-${var.region}-${var.project_id}"
-  location = var.region # Use variable to determine location
+  name                        = "gcp-simple-global-web-app-demo-${var.project_id}"
+  location = "US" # Set a consistent location for simplicity
   uniform_bucket_level_access = true
 
   website {
-
     main_page_suffix = "index.html"
     not_found_page   = "404.html"
-
   }
 }
 
 resource "google_storage_bucket_object" "object_us" {
     name = "image_bullet.jpg"
-    bucket = google_storage_bucket.bucket["us_central1"].name
+    bucket = google_storage_bucket.default.name
 
     source = "${path.module}/images/image_bullet.jpg"
 
@@ -25,7 +21,7 @@ resource "google_storage_bucket_object" "object_us" {
 resource "google_storage_bucket_object" "object_europe" {
     name = "image_dome.jpg"
 
- bucket = google_storage_bucket.bucket["europe_west1"].name
+ bucket = google_storage_bucket.default.name
     source = "${path.module}/images/image_dome.jpg"
 
 }
@@ -33,7 +29,7 @@ resource "google_storage_bucket_object" "object_europe" {
 resource "google_storage_bucket_object" "object_asia" {
 
     name = "image_asia.jpg"
- bucket = google_storage_bucket.bucket["asia_southeast1"].name
-    source = "${path.module}/images/image_eye.jpg"
+ bucket = google_storage_bucket.default.name
+    source = "${path.module}/images/image_asia.jpg"
 
 }
