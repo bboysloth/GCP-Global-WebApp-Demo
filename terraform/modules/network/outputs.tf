39a739ee3e04f60a5fa1cfa8f4e5775c5bda8e36
bboysloth <jeremyhannan@gmail.com>
2024-12-09 08:35:08 -0700

    Update outputs.tf

--- terraform/modules/network/outputs.tf
+++ terraform/modules/network/outputs.tf
@@ -1,15 +1,11 @@
-output "network_id" {
-  value = google_compute_network.vpc_network.id
-}
-
 output "subnet_us_id" {
   value = google_compute_subnetwork.subnet_us.id
 }
 
 output "subnet_europe_id" {
-  value = google_compute_subnetwork.subnet_europe.id
+ value = google_compute_subnetwork.subnet_europe.id
 }
 
 output "subnet_asia_id" {
-  value = google_compute_subnetwork.subnet_asia.id
+ value = google_compute_subnetwork.subnet_asia.id
 }
