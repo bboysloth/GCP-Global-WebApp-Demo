5906f2387fcc3f294f4362fc985d5d9a9d77316b
bboysloth <jeremyhannan@gmail.com>
2024-12-08 20:41:33 -0700

    Update outputs.tf
    overhaul update for outputs

--- terraform/modules/compute/outputs.tf
+++ terraform/modules/compute/outputs.tf
@@ -1,19 +1,7 @@
 output "instance_template_id" {
-  value = google_compute_instance_template.default.id
+    value = google_compute_instance_template.default.self_link
 }
 
-output "mig_us_self_link" {
-  value = google_compute_region_instance_group_manager.us_mig.self_link
-}
-
-output "mig_europe_self_link" {
-  value = google_compute_region_instance_group_manager.europe_mig.self_link
-}
-
-output "mig_us_instance_group" {
-    value = google_compute_region_instance_group_manager.us_mig.instance_group
-}
-
-output "mig_europe_instance_group" {
-    value = google_compute_region_instance_group_manager.europe_mig.instance_group
+output "instance_group" {
+    value = google_compute_region_instance_group_manager.mig.instance_group
 }
