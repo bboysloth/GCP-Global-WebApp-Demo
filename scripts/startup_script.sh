7f192023bc2cc8d28c0d7bcf2bf30d957f1ca4e4
bboysloth <jeremyhannan@gmail.com>
2024-12-08 18:14:45 -0700

    Update startup_script.sh
    edited to account of TF errors

--- scripts/startup_script.sh
+++ scripts/startup_script.sh
@@ -2,6 +2,8 @@
 sudo apt-get update
 sudo apt-get install apache2 -y
 sudo a2enmod rewrite
+
+# Use a here-document to create the Apache configuration file
 cat <<EOT > /etc/apache2/sites-available/000-default.conf
 <VirtualHost *:80>
     ServerAdmin webmaster@localhost
@@ -9,13 +11,19 @@ cat <<EOT > /etc/apache2/sites-available/000-default.conf
 
     <Directory /var/www/html/>
         RewriteEngine On
-        RewriteCond %{REQUEST_URI} !^/index.html$
+        RewriteCond %{REQUEST_URI} !^/index\.html$
         RewriteRule ^(.*)$ /index.html [L]
     </Directory>
 
-    ErrorLog ${APACHE_LOG_DIR}/error.log
-    CustomLog ${APACHE_LOG_DIR}/access.log combined
+    ErrorLog \${APACHE_LOG_DIR}/error.log
+    CustomLog \${APACHE_LOG_DIR}/access.log combined
 </VirtualHost>
 EOT
+
 sudo systemctl restart apache2
-echo "<h1>Hello from $(hostname) in $(curl -s http://metadata.google.internal/computeMetadata/v1/instance/zone -H 'Metadata-Flavor: Google')</h1><img src='https://storage.googleapis.com/gcp-simple-global-web-app-demo-us-${project_id}/image_bullet.jpg'>" > /var/www/html/index.html
+
+# Construct the image URL using the project_id variable
+IMAGE_URL="https://storage.googleapis.com/gcp-simple-global-web-app-demo-us-${project_id}/image_bullet.jpg"
+
+# Create the index.html file with the image URL
+echo "<h1>Hello from $(hostname) in $(curl -s http://metadata.google.internal/computeMetadata/v1/instance/zone -H 'Metadata-Flavor: Google')</h1><img src='${IMAGE_URL}'>" > /var/www/html/index.html
