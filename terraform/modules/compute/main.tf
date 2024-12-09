a9d8cb2b16cfab9fdaf1e3748a1b5593aa3b1d55
bboysloth <jeremyhannan@gmail.com>
2024-12-09 08:29:05 -0700

    Update main.tf
    restart

--- terraform/modules/compute/main.tf
+++ terraform/modules/compute/main.tf
@@ -1,5 +1,5 @@
 resource "google_compute_instance_template" "default" {
-  name_prefix  = "web-server-template-${var.region}-" # Make names unique per region
+  name_prefix  = "web-server-template-${var.region}-"
   machine_type = "e2-micro"
   disk {
     source_image = "debian-cloud/debian-11"
@@ -7,27 +7,29 @@ resource "google_compute_instance_template" "default" {
     boot         = true
   }
   network_interface {
-    subnetwork = var.subnet_id
+
   }
 
+  # Startup script (using templatefile)
   metadata = {
-    startup-script = templatefile("${path.module}/../../../scripts/startup_script.sh", {
-      project_id = var.project_id, # Make sure this is passed to the script
-      region     = var.region
+    startup-script = templatefile("${path.module}/../../../scripts/startup_script.sh",
+      {
+        project_id = var.project_id,
+        region     = var.region,
     })
   }
 }
 
-resource "google_compute_region_instance_group_manager" "mig" { # Removed us and europe from name
-  provider              = google-beta
-  name                  = "web-server-mig-${var.region}"  # Make names unique per region
-  region                = var.region
-  project               = var.project_id
-  wait_for_instances    = false
-  base_instance_name    = "web-server-${var.region}"
-
+resource "google_compute_region_instance_group_manager" "mig" {
+  provider = google-beta
+    project = var.project_id
+  name     = "web-server-mig-${var.region}"
+  region   = var.region
+ wait_for_instances = false
   version {
-    name              = "primary"
     instance_template = google_compute_instance_template.default.id
+    name              = "primary"
   }
+
+  base_instance_name = "web-server-${var.region}"
 }
