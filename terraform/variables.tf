92121618b97dab476cd3be11051757789dfd9140
bboysloth <jeremyhannan@gmail.com>
2024-12-08 21:31:16 -0700

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,15 +1,1 @@
-variable "project_id" {
-  description = "The ID of the GCP project"
-  type        = string
-}
-
-variable "region" {
-  description = "The default region for resources"
-  type        = string
-  default     = "us-central1"
-}
-
-variable "project_number" {
-  description = "The project number for the project"
-  type = number
-}
+#empty
