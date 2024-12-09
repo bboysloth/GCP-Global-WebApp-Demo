0047b73a85d9ffc3e4e2c24380e68446ed7c91e4
bboysloth <jeremyhannan@gmail.com>
2024-12-09 10:09:25 -0700

    Update routes.tf

--- terraform/modules/network/routes.tf
+++ terraform/modules/network/routes.tf
@@ -1,29 +1,25 @@
 resource "google_compute_router" "router" {
-  name     = "terraform-demo-router"
-  region   = "us-central1"
-  project  = var.project_id
-  network  = google_compute_network.vpc_network.id
-
+  name    = "terraform-demo-router"
+  region  = "us-central1"
+  project = var.project_id
+  network = google_compute_network.vpc_network.id
 }
 
 resource "google_compute_router_nat" "nat" {
   name                               = "terraform-demo-nat"
   router                             = google_compute_router.router.name
   region                             = google_compute_router.router.region
-  project                            = var.project_id
+ project                            = var.project_id
   nat_ip_allocate_option             = "AUTO_ONLY"
   source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
 }
 
 # Default route to the internet gateway
 resource "google_compute_route" "default_internet_gateway" {
-
-    project = var.project_id
- name = "default-internet-gateway"
+ project = var.project_id
+ name        = "default-internet-gateway"
  dest_range = "0.0.0.0/0"
- network = google_compute_network.vpc_network.name
-
- next_hop_ilb = google_compute_router_nat.nat.id
-
- priority = 1000
+ network    = google_compute_network.vpc_network.name
+ next_hop_ilb = google_compute_router_nat.nat.self_link # Corrected to use self_link
+ priority    = 1000
 }
