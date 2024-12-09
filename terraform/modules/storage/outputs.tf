f20a84d068c8eec36640caaf279e110caa9d7c9b
bboysloth <jeremyhannan@gmail.com>
2024-12-08 21:25:21 -0700

    Update outputs.tf

--- terraform/modules/storage/outputs.tf
+++ terraform/modules/storage/outputs.tf
@@ -1,11 +1,3 @@
-output "bucket_self_link_us" {
-    value = google_storage_bucket.bucket["us_central1"].self_link
-}
-
-output "bucket_self_link_europe" {
-    value = google_storage_bucket.bucket["europe_west1"].self_link
-
-}
-output "bucket_self_link_asia" {
-    value = google_storage_bucket.bucket["asia_southeast1"].self_link
+output "bucket_self_link" {
+    value = google_storage_bucket.default.self_link
 }
