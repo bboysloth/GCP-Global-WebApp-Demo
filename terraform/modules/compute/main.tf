e8e50b6bf98d66ced82988a9dfd356c3394e3a64
bboysloth <jeremyhannan@gmail.com>
2024-12-08 18:33:32 -0700

    Update main.tf
    test around template file

--- terraform/modules/compute/main.tf
+++ terraform/modules/compute/main.tf
@@ -1,5 +1,5 @@
 data "template_file" "startup_script" {
-  template = file("${path.module}/../../../scripts/startup_script.sh")
+  template = templatefile("${path.module}/../../../scripts/startup_script.sh")
   vars = {
     project_id = var.project_id
   }
