dee25c9187a277d8c156bdbe752b993e88ef709f
bboysloth <jeremyhannan@gmail.com>
2024-12-10 14:38:02 -0700

    Update outputs.tf
    added health check id to output for other modules consumption

--- terraform/modules/loadbalancer/outputs.tf
+++ terraform/modules/loadbalancer/outputs.tf
@@ -3,3 +3,7 @@
 output "load_balancer_ip" {
   value = google_compute_global_address.web_server_lb_ip.address
 }
+
+output "http_health_check_id" {
+  value = google_compute_http_health_check.http_health_check.id
+}
