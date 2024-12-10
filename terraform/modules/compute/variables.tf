3d6bf605065a9d54ef5ac28f9e77b3bf6cf91517
bboysloth <jeremyhannan@gmail.com>
2024-12-10 14:39:09 -0700

    Update variables.tf
    added health check for auto-healing

--- terraform/modules/compute/variables.tf
+++ terraform/modules/compute/variables.tf
@@ -12,3 +12,8 @@ variable "subnet_id" {
   type        = string
   description = "The subnet ID for compute instances"
 }
+
+variable "health_check_id" { # Add the health_check_id variable for auto-healing config
+  type        = string
+  description = "ID of the health check to use for autohealing"
+}
