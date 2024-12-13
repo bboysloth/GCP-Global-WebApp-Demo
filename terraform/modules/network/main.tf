5adbc666470b4db1dfb40009bee36743ac235821
bboysloth <jeremyhannan@gmail.com>
2024-12-13 16:39:41 -0700

    Update main.tf
    updated spacing for clarity

--- terraform/modules/network/main.tf
+++ terraform/modules/network/main.tf
@@ -6,27 +6,27 @@ resource "google_compute_network" "vpc_network" {
 
 resource "google_compute_subnetwork" "subnet_us" {
   project                    = var.project_id
-  name                        = "terraform-demo-subnet-us"
+  name                       = "terraform-demo-subnet-us"
   ip_cidr_range              = "10.0.1.0/24"
-  region = "us-central1"
-  network       = google_compute_network.vpc_network.id
+  region                     = "us-central1"
+  network                    = google_compute_network.vpc_network.id
 }
 
 resource "google_compute_subnetwork" "subnet_europe" {
   project                    = var.project_id
-  name = "terraform-demo-subnet-europe"
+  name                       = "terraform-demo-subnet-europe"
   ip_cidr_range              = "10.0.2.0/24"
- region = "europe-west1"
-  network       = google_compute_network.vpc_network.id
+ region                      = "europe-west1"
+  network                    = google_compute_network.vpc_network.id
 
 }
 
 resource "google_compute_subnetwork" "subnet_asia" {
  project                    = var.project_id
-  name          = "terraform-demo-subnet-asia"
+ name                       = "terraform-demo-subnet-asia"
  ip_cidr_range              = "10.0.3.0/24"
- region = "asia-southeast1"
-  network       = google_compute_network.vpc_network.id
+ region                     = "asia-southeast1"
+ network                    = google_compute_network.vpc_network.id
 }
 
 
